97eed3fac663db4df95b9bc5f213172e0f67463b
Cathella <ncathie214@gmail.com>
2021-03-31 16:28:04 +0300

    style rrot index view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @projects = current_user.projects.all
+    # if user_signed_in?
+    #   @projects = current_user.projects.all
+    # end
   end
 end
